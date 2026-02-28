9d21315c5543d72fa2d5b0b9583b734b1d9d3384
vanidicto <emanrabusamiranda@gmail.com>
2026-02-28 12:12:18 +0800

    ReportCurrents

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -25,7 +25,7 @@ const Report = () => {
       collection(db, 'reports'),
       where('appUserId', '==', currentUser.uid)
     );
-    
+
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const fetchedReports = snapshot.docs.map(doc => ({
         id: doc.id,
@@ -58,10 +58,10 @@ const Report = () => {
         avatar: user.avatar
       },
       // Since reports are generated when SOS turns OFF, we treat them as "Resolved"
-      displayStatus: { 
+      displayStatus: {
         text: 'Marked Safe',
-        color: 'red', 
-        icon: ShieldAlert 
+        color: 'red',
+        icon: ShieldAlert
       },
       // Map fields for the modal
       pulse: `${report.pulseRate} bpm`,
@@ -77,7 +77,7 @@ const Report = () => {
     setSelectedIncident(null);
   };
 
-  if(!reports.length) return (
+  if (!reports.length) return (
     <LoadingSpinner />
   )
 
@@ -88,7 +88,7 @@ const Report = () => {
           {reports.map(rawReport => {
             const incident = getEnrichedReport(rawReport);
             const IconComponent = incident.displayStatus.icon;
-            
+
             return (
               <li
                 key={incident.id}
@@ -104,23 +104,23 @@ const Report = () => {
                 }}
               >
                 <div className="incident-link-wrapper">
-                    <div className={`incident-icon-wrapper ${incident.displayStatus.color}`}>
-                        <IconComponent size={20} />
-                    </div>
+                  <div className={`incident-icon-wrapper ${incident.displayStatus.color}`}>
+                    <IconComponent size={20} />
+                  </div>
 
-                    <div className="incident-details">
-                        <p className={`incident-type ${incident.displayStatus.color}`}>{incident.displayStatus.text}</p>
-                        <p className="incident-date-location">
-                        {incident.date} &bull; {incident.location}
-                        </p>
-                    </div>
+                  <div className="incident-details">
+                    <p className={`incident-type ${incident.displayStatus.color}`}>{incident.displayStatus.text}</p>
+                    <p className="incident-date-location">
+                      {incident.date} &bull; {incident.location}
+                    </p>
+                  </div>
 
-                    <div className="incident-time-wrapper">
-                        <span className="incident-date-right">
-                            {incident.time}
-                        </span>
-                        <ChevronRight size={20} className="chevron-icon" />
-                    </div>
+                  <div className="incident-time-wrapper">
+                    <span className="incident-date-right">
+                      {incident.time}
+                    </span>
+                    <ChevronRight size={20} className="chevron-icon" />
+                  </div>
                 </div>
               </li>
             );
